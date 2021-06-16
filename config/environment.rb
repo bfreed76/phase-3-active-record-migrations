ab0bef757e194f672db2dac03d9eab748005a90f
Brinton <brintonfoyreed@gmail.com>
2021-06-16 08:57:25 -0700

    commit tree 2dbafc845daf4baac8572a72f50314341f0f9dd1

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,11 @@ require 'date'
 require 'bundler/setup'
 Bundler.require
 
-
 # put the code to connect to the database here
 
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3", #what time of sql is this type of db?
+    database: "db/artists.sqlite" #will create db if not found
+)
 
 require_relative "../artist.rb"
